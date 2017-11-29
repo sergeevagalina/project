d589c1cc1a1363ba8eaf0e694def8952de77f4b4
Galina Sergeeva <sergeevagalina.al@gmail.com>
2017-11-29 12:38:48 +0300

    Refactored app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { LocalStorageModule } from 'angular-2-local-storage';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing/app-routing.module';
+import { fakeBackendProvider } from './helpers/fake-backend';
+import { MockBackend, MockConnection} from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { routing } from './app-routing';
+
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { UserprofileComponent } from './userprofile/userprofile.component';
@@ -14,6 +19,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { UserService } from './services/user.service';
+import { AuthenticationService } from './services/authentication.service';
 
 @NgModule({
   declarations: [
@@ -27,15 +33,21 @@ import { UserService } from './services/user.service';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
+    HttpModule,
+    routing,
     NgbModule.forRoot(),
     LocalStorageModule.withConfig({
       prefix: 'my-app',
       storageType: 'localStorage'
-    }),
-    AppRoutingModule
+    })
   ],
-  providers: [UserService],
+  providers: [
+    UserService,
+    AuthenticationService,
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
